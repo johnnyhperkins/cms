fca969b7c6167729ffad27b4f75770e33a6f5a47
stephenchung27 <me@stephenchung.io>
2019-04-04 16:32:09 -0400

    Implement input prototype

---
+++ frontend/src/components/form_creator/sidebar/form_input.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../styles/form_input.scss';
+
+const FormInput = () => {
+  return (
+    <div className='form-input'>
+      <input type='text' value='Text Field' readOnly/>
+    </div>
+  )
+}
+
+export default FormInput;
--- frontend/src/components/form_creator/sidebar/sidebar.tsx
+++ frontend/src/components/form_creator/sidebar/sidebar.tsx
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import '../../../styles/sidebar.scss';
 import logo from '../../../images/borderbox-logo.svg';
+import FormInput from './form_input';
 
 const SideBar = () => {
   const [isHidden, setIsHidden] = useState(false);
 
   return (
-    <div 
-    className={'sidebar' + (isHidden ? ' hidden' : '')}>
-        <button onClick={() => setIsHidden(!isHidden)}>
-          <i className='material-icons'>navigate_next</i>
-        </button>
+    <div
+      className={'sidebar' + (isHidden ? ' hidden' : '')}>
+      <button onClick={() => setIsHidden(!isHidden)} className='side-bar-toggle'>
+        <i className='material-icons'>navigate_next</i>
+      </button>
       <header>
         <img className='logo' src={logo} />
         <h3>Form title</h3>
       </header>
       <main>
-        <p>Hello</p>
+        <FormInput />
       </main>
       <footer>
-        <p>Bye</p>
+        <button>Save</button>
+        <button>Cancel</button>
       </footer>
     </div>
   );
